674dc4cfacc6acd7b479f3d03449162379a62037
Sergio <sergionet17@gmail.com>
2017-10-02 11:53:42 -0500

    cambio con script

--- git/services/testService.js
+++ git/services/testService.js
@@ -1,6 +1,6 @@
 angular.module('testService', [])//Declaramos el modulo
 	.factory('testRequest', function($http) { //declaramos la factory
-		var path = "http://localhost:8080/campana";//API path
+		var path = "http://localhost:8080/campanas";//API path
 		return {
 			//Login
 			posts : function(){ //Retornara la lista de posts
